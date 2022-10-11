d6b424b8b7649ed119eb5268d870f5e0438140d2
pawel-krakowiak <furajchirashi3003@gmail.com>
2022-10-11 17:46:14 +0200

    Added location model

--- amb_app/models.py
+++ amb_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 
+# TODO: Reward for cities and users
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=32, unique=True)
 
 class Reward(models.Model):
     """Reawrd model"""
@@ -10,6 +15,7 @@ class Reward(models.Model):
     reward_img = models.ImageField(upload_to="img_rewards", blank=False, default="img_rewards/not_found.png")
     points_price = models.DecimalField(max_digits=10, decimal_places=0, blank=False)
     is_available = models.BooleanField(blank=False, default=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=False)
     
     def __str__(self):
         return self.name_with_price()
@@ -51,7 +57,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
@@ -60,6 +65,7 @@ class User(AbstractBaseUser):
     name = models.CharField(max_length=64, blank=False)
     surename = models.CharField(max_length=64, blank=False)
     instagram_name = models.CharField(max_length=64, blank=False, unique=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=False)
     
     objects = UserManager()
     
