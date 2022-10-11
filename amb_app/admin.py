cdecc2efd1a600eeb0a09e6eae1e2543cc860adc
pawel-krakowiak <furajchirashi3003@gmail.com>
2022-10-11 15:48:11 +0200

    Registered admin reward

--- amb_app/admin.py
+++ amb_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reward
 
-# Register your models here.
+@admin.register(Reward)
+class RewardAdmin(admin.ModelAdmin):
+    list_filter = ['points_price']
+    search_fields = ['name']
