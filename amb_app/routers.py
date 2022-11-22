28ce05a85c31e3ba68b494e8ebe2355f2c018c59
pawel-krakowiak <furajchirashi3003@gmail.com>
2022-11-22 16:34:39 +0100

    Separated api urls into routers

---
+++ amb_app/routers.py
@@ -0,0 +1,17 @@
+from rest_framework.routers import SimpleRouter
+from amb_app.views import UserViewSet, LoginViewSet, RefreshViewSet, LocationViewSet, RewardViewSet
+
+routes = SimpleRouter()
+
+routes.register(r'auth/login', LoginViewSet, basename='auth-login')
+routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+# USER
+routes.register(r'users', UserViewSet, basename='users')
+routes.register(r'locations', LocationViewSet, basename='locations')
+routes.register(r'rewards', RewardViewSet, basename='rewards')
+
+
+urlpatterns = [
+    *routes.urls
+]
--- ambassadors_panel/urls.py
+++ ambassadors_panel/urls.py
@@ -2,19 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers
-from amb_app.views import UserViewSet, LocationViewSet, RewardViewSet, LoginViewSet, RefreshViewSet
+# from rest_framework import router
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
-router.register(r'locations', LocationViewSet, basename='locations')
-router.register(r'rewards', RewardViewSet, basename='rewards')
-router.register(r'auth/login', LoginViewSet, basename='auth-login')
-router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+# router = router.DefaultRouter()
 
 urlpatterns = [
     path('jet/', include('jet.urls')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+    path('api/', include(('amb_app.routers', 'core'), namespace='core-api')),
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
