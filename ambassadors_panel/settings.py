c1ebf1b1126703d736788b2378c57b72b50a1bfc
pawel-krakowiak <furajchirashi3003@gmail.com>
2022-11-29 14:30:13 +0100

    Added corse middleware

--- ambassadors_panel/settings.py
+++ ambassadors_panel/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
 
 # Application definition
 
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
     'jet.dashboard',
     'jet',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
