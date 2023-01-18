07124fd3d5466eb921246e6f3bfe86b89e397d16
pawel-krakowiak <furajchirashi3003@gmail.com>
2023-01-18 13:55:57 +0100

    Migrated DB to live Postgress

--- ambassadors_panel/settings.py
+++ ambassadors_panel/settings.py
@@ -108,10 +108,21 @@ WSGI_APPLICATION = 'ambassadors_panel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': config("PGPASSWORD"),
+        'HOST': 'containers-us-west-133.railway.app',
+        'PORT': '7713'
     }
 }
 
