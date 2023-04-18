6ea0dbb69b52f20d7563f6ed26a83617168f1ab8
pawel-krakowiak <furajchirashi3003@gmail.com>
2023-04-18 15:42:05 +0200

    Temp serializers change

--- amb_app/serializers.py
+++ amb_app/serializers.py
@@ -5,7 +5,6 @@ from rest_framework_simplejwt.settings import api_settings
 from django.contrib.auth.models import update_last_login
 
 class LoginSerializer(TokenObtainPairSerializer):
-    
     def validate(self, attrs):
         data = super().validate(attrs)
         
@@ -16,15 +15,17 @@ class LoginSerializer(TokenObtainPairSerializer):
         data['access'] = str(refresh.access_token)
         
         if api_settings.UPDATE_LAST_LOGIN:
-            update_last_Login(None, self.user)
+            update_last_login(None, self.user)
 
         return data
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):    
     class Meta:
         model = User
-        fields = ['id', 'email', 'points', 'name', 'surename', 'instagram_name', 'location', 'is_active', 'admin', 'staff', 'created', 'updated']
-        read_only_field = ['is_active', 'created', 'udated']
+        # fields = ['id', 'email', 'points', 'name', 'surename', 'instagram_name', 'location', 'is_active', 'admin', 'staff', 'created', 'updated']
+        # read_only_field = ['is_active', 'created', 'updated']
+        fields = ['id', 'email', 'points', 'name', 'surename', 'instagram_name', 'location', 'is_active', 'admin', 'staff']
+        read_only_field = ['is_active']
         
 class RewardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
