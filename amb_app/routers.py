b86863c804499171cb6bceb460ba4778b688dd92
pawel-krakowiak <furajchirashi3003@gmail.com>
2023-06-16 15:36:22 +0200

    Defined user history routing

--- amb_app/routers.py
+++ amb_app/routers.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import SimpleRouter
-from amb_app.views import UserViewSet, LoginViewSet, RefreshViewSet, LocationViewSet, RewardViewSet
+from amb_app.views import UserViewSet, LoginViewSet, RefreshViewSet, LocationViewSet, RewardViewSet, UserActionHistoryViewSet
 
 routes = SimpleRouter()
 
@@ -10,6 +10,7 @@ routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 routes.register(r'users', UserViewSet, basename='users')
 routes.register(r'locations', LocationViewSet, basename='locations')
 routes.register(r'rewards', RewardViewSet, basename='rewards')
+routes.register(r'user-history', UserActionHistoryViewSet, basename='user-history')
 
 
 urlpatterns = [
