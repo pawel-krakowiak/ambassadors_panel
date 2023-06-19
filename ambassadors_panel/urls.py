161a8741dc176e5a34cb0eb2d77e8989bb648f17
pawel-krakowiak <furajchirashi3003@gmail.com>
2023-06-19 15:49:27 +0200

    Added urls

--- ambassadors_panel/urls.py
+++ ambassadors_panel/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 # from rest_framework import router
 
 # router = router.DefaultRouter()
@@ -11,5 +12,8 @@ urlpatterns = [
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('api/', include(('amb_app.routers', 'core'), namespace='core-api')),
     path('admin/', admin.site.urls),
-    # path('', include(router.urls)),
+    path('reset-password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset-password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset-password/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
