6f97696e72a90ca693e1e9327d297c9809fcae70
pawel-krakowiak <furajchirashi3003@gmail.com>
2023-06-20 19:57:15 +0200

    Refactored urls

--- ambassadors_panel/urls.py
+++ ambassadors_panel/urls.py
@@ -3,9 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-# from rest_framework import router
-
-# router = router.DefaultRouter()
 
 urlpatterns = [
     path('jet/', include('jet.urls')),
