687f5e0c4900fb345ebe888b6ca425df065b7447
pawel-krakowiak <furajchirashi3003@gmail.com>
2023-06-20 19:52:24 +0200

    Refactored views

--- amb_app/views.py
+++ amb_app/views.py
@@ -5,65 +5,54 @@ from .serializers import UserSerializer, RewardSerializer, LocationSerializer, L
 from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
-from rest_framework.permissions import IsAuthenticated
 
-class LoginViewSet(viewsets.ModelViewSet, TokenObtainPairView):
+class LoginViewSet(TokenObtainPairView, viewsets.ModelViewSet):
     serializer_class = LoginSerializer
     permission_classes = (AllowAny,)
     http_method_names = ['post']
-    
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        
         try:
             serializer.is_valid(raise_exception=True)
         except TokenError as token_error:
             raise InvalidToken(token_error.args[0])
-    
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
-    
+
 class RefreshViewSet(viewsets.ModelViewSet):
     serializer_class = RefreshSerializer
     http_method_names = ['post']
     permission_classes = (IsAuthenticated,)
-    
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        
         try:
             serializer.is_valid(raise_exception=True)
         except TokenError as token_error:
             raise InvalidToken(token_error.args[0])
-        
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 class UserViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
     permission_classes = (IsAuthenticated,)
-    fielter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.OrderingFilter]
     ordering_fields = ['updated']
     ordering = ['-updated']
     serializer_class = UserSerializer
-    
+
     def get_queryset(self):
         if self.request.user.is_admin:
             return User.objects.all()
-        
-    def get_object(self):
-        obj = User.objects.get(pk=self.kwargs[self.lookup_field])
-        self.check_object_permissions(self.request, obj)
-        
-        return obj
+        return User.objects.filter(id=self.request.user.id)
 
     @action(detail=False, methods=['get'])
     def get_user_info(self, request):
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data)
-    
+
 class RewardViewSet(viewsets.ModelViewSet):
     queryset = Reward.objects.all()
     serializer_class = RewardSerializer
@@ -71,23 +60,17 @@ class RewardViewSet(viewsets.ModelViewSet):
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
-    
+
 class UserActionHistoryViewSet(viewsets.ModelViewSet):
     queryset = UserActionHistory.objects.all()
     serializer_class = UserActionHistorySerializer
-    
+
     @action(detail=False, methods=['get'])
-    def get_user_history(self, request, user_id=None):
-        if user_id is not None:
-            user_actions = UserActionHistory.objects.filter(user_id=user_id)
-        else:
-            user_actions = UserActionHistory.objects.none()
-        
+    def get_user_history(self, request):
+        user_id = request.user.id
+        user_actions = UserActionHistory.objects.filter(user_id=user_id)
         serializer = UserActionHistorySerializer(user_actions, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        return self.get_user_history(request, user_id=pk)
-
-# Create your views here.
-
+        return self.get_user_history(request)
